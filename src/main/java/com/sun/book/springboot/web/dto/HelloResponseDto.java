0a30afbb41775f4b7ced3f5f1dd82639caf8e9fa
choisun <choisun0228@gamil.com>
2020-08-12 22:07:01 +0900

    lombok 사용

---
+++ src/main/java/com/sun/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.sun.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    public final String name;
+    public final int amount;
+}
